0b15ae44e59182b9034e812b6ea3bc8d0e33db6d
sporejack <jackmcpherson@mycelium.ventures>
2021-03-10 10:04:07 +1000

    Add additional unit tests
    This commit adds additional unit tests for the Trader shim.

--- test-ts/unit/Trader.ts
+++ test-ts/unit/Trader.ts
@@ -1,5 +1,6 @@
 //@ts-ignore
 import { expectRevert } from "@openzeppelin/test-helpers"
+import assert from "assert"
 import { setupContractsAndTracer } from "../lib/Setup"
 import { TracerInstance, TraderInstance } from "../../types/truffle-contracts"
 import { signOrder, signOrders, domain, domainData, limitOrder } from "../lib/Signing"
@@ -12,6 +13,7 @@ describe("Trader Shim unit tests", async () => {
     
     let sampleMakers: any;
     let sampleTakers: any;
+    let badMakers: any;
 
     before(async () => {
         await configure()
@@ -22,49 +24,68 @@ describe("Trader Shim unit tests", async () => {
         tracer = await deployed.tracer;
         trader = await deployed.trader;
     
-    sampleMakers = [
-        {
-            amount: "5000000000000000000",
-            price: "100000000",
-            side: true,
-            user: accounts[1],
-            expiration: 1598590237,
-            targetTracer: tracer.address,
-            nonce: 18,
-        },
-        {
-            amount: "5000002200000000000",
-            price: "100000088",
-            side: false,
-            user: accounts[0],
-            expiration: 1598590909,
-            targetTracer: tracer.address,
-            nonce: 33,
-        }
-    ];
+        sampleMakers = [
+            {
+                amount: "5000000000000000000",
+                price: "100000000",
+                side: true,
+                user: accounts[1],
+                expiration: 1598590237,
+                targetTracer: tracer.address,
+                nonce: 18,
+            },
+            {
+                amount: "5000002200000000000",
+                price: "100000088",
+                side: false,
+                user: accounts[0],
+                expiration: 1598590909,
+                targetTracer: tracer.address,
+                nonce: 33,
+            }
+        ];
+    
+        sampleTakers = [
+            {
+                amount: "5000000000000000000",
+                price: "100000000",
+                side: true,
+                user: accounts[2],
+                expiration: 1598590237,
+                targetTracer: tracer.address,
+                nonce: 18,
+            },
+            {
+                amount: "5000002200000000000",
+                price: "100000088",
+                side: false,
+                user: accounts[2],
+                expiration: 1598590909,
+                targetTracer: tracer.address,
+                nonce: 33,
+            }
+        ];
 
-    sampleTakers = [
-        {
-            amount: "5000000000000000000",
-            price: "100000000",
-            side: true,
-            user: accounts[2],
-            expiration: 1598590237,
-            targetTracer: tracer.address,
-            nonce: 18,
-        },
-        {
-            amount: "5000002200000000000",
-            price: "100000088",
-            side: false,
-            user: accounts[2],
-            expiration: 1598590909,
-            targetTracer: tracer.address,
-            nonce: 33,
-        }
-    ];
-
-
+        badMakers = [
+            {
+                amount: "5000000000000000000",
+                price: "100000011",
+                side: true,
+                user: accounts[1],
+                expiration: 1598590237,
+                targetTracer: tracer.address,
+                nonce: 18,
+            },
+            {
+                amount: "5000002200000000000",
+                price: "100000088",
+                side: false,
+                user: accounts[0],
+                expiration: 1598590909,
+                targetTracer: tracer.address,
+                nonce: 33,
+            }
+        ];
     })
 
     describe("executeTrade", () => {
@@ -91,7 +112,40 @@ describe("Trader Shim unit tests", async () => {
                 let takers: any = [];
                 let market: string = tracer.address;
 
-                await expectRevert(trader.executeTrade(makers, takers, market), "TDR: Received empty arrays")
+                await expectRevert(
+                    trader.executeTrade(makers, takers, market),
+                    "TDR: Received empty arrays")
+            })
+        })
+
+        context("When there is a price mismatch", () => {
+            it("reverts", async () => {
+                let makers: any = badMakers;
+                let takers: any = sampleTakers;
+                let market: string = tracer.address;
+
+                /* sign orders for submission */
+                let signedMakers: any = await Promise.all(await signOrders(web3, makers, domain, await domainData(trader.address), limitOrder));
+                let signedTakers: any = await Promise.all(await signOrders(web3, takers, domain, await domainData(trader.address), limitOrder));
+
+                await expectRevert(
+                    trader.executeTrade(signedMakers, signedTakers, market),
+                    "TDR: Price mismatch"
+                )
+            })
+        })
+
+        context("When both input arrays are valid", () => {
+            it("passes", async () => {
+                let makers: any = sampleMakers;
+                let takers: any = sampleTakers;
+                let market: string = tracer.address;
+
+                /* sign orders for submission */
+                let signedMakers: any = await Promise.all(await signOrders(web3, makers, domain, await domainData(trader.address), limitOrder));
+                let signedTakers: any = await Promise.all(await signOrders(web3, takers, domain, await domainData(trader.address), limitOrder));
+
+                assert(trader.executeTrade(signedMakers, signedTakers, market));
             })
         })
     })
