cdb3d7c45765eb738cabaf4fb60cb7685fc9bd43
BenjaminPatch <benpatch@hotmail.com>
2021-03-25 10:42:08 +1000

    Test for Account.withdrawERC20

--- contracts/Interfaces/ISafetyWithdraw.sol
+++ contracts/Interfaces/ISafetyWithdraw.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.6.12;
 pragma experimental ABIEncoderV2;
 
 interface ISafetyWithdraw {
-    function withdrawERC20Token(address tokenAddress, address to, uint256 amount);
+    function withdrawERC20Token(address tokenAddress, address to, uint256 amount) external;
 }
