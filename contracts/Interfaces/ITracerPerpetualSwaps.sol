833da8e94a5d633c3a70ed1874c475845335a5ce
Raymond Mogg <raymondmogg@gmail.com>
2021-05-04 12:44:39 +1000

    Move logic from Tracer to Pricing (#50)
    * migrate timing over to pricing contract
    
    * remove oracle from tracerperp and put into pricing
    
    * add oracle decimals as perp constructor arg
    
    * remove pricing bloat
    
    * remove comment

--- contracts/Interfaces/ITracerPerpetualSwaps.sol
+++ contracts/Interfaces/ITracerPerpetualSwaps.sol
@@ -22,8 +22,6 @@ interface ITracerPerpetualSwaps {
 
     function leveragedNotionalValue() external view returns(int256);
 
-    function oracle() external view returns(address);
-
     function gasPriceOracle() external view returns(address);
 
     function priceMultiplier() external view returns(uint256);
@@ -36,15 +34,11 @@ interface ITracerPerpetualSwaps {
 
     function fundingRateSensitivity() external view returns(uint256);
 
-    function currentHour() external view returns(uint8);
-
     function getBalance(address account) external view returns (Types.AccountBalance memory);
 
     function setInsuranceContract(address insurance) external;
 
     function setPricingContract(address pricing) external;
-
-    function setOracle(address _oracle) external;
 
     function setGasOracle(address _gasOracle) external;
 
@@ -55,8 +49,6 @@ interface ITracerPerpetualSwaps {
     function setFundingRateSensitivity(uint256 _fundingRateSensitivity) external;
 
     function transferOwnership(address newOwner) external;
-
-    function initializePricing() external;
 
     function matchOrders(Types.Order memory order1, Types.Order memory order2, uint256 fillAmount) external;
 }
--- contracts/TracerPerpetualSwaps.sol
+++ contracts/TracerPerpetualSwaps.sol
@@ -23,25 +23,18 @@ contract TracerPerpetualSwaps is
 
 	uint256 public override fundingRateSensitivity;
 	uint256 public constant override LIQUIDATION_GAS_COST = 63516;
+	// todo ensure these are fine being immutable
 	uint256 public immutable override priceMultiplier;
 	address public immutable override tracerBaseToken;
 	bytes32 public immutable override marketId;
-	IAccount public accountContract;
 	IPricing public pricingContract;
 	IInsurance public insuranceContract;
 	address public liquidationContract;
 	uint256 public override feeRate;
 
 	// Config variables
-	address public override oracle;
 	address public override gasPriceOracle;
-	bool private pricingInitialized;
 	int256 public override maxLeverage; // The maximum ratio of notionalValue to margin
-
-	// Funding rate variables
-	uint256 internal startLastHour;
-	uint256 internal startLast24Hours;
-	uint8 public override currentHour;
 
 	// Account State Variables
 	mapping(address => Types.AccountBalance) public balances;
@@ -52,12 +45,6 @@ contract TracerPerpetualSwaps is
 	mapping(address => bool) tradingWhitelist;
 
 	event FeeReceiverUpdated(address receiver);
-	event HourlyPriceUpdated(int256 price, uint256 currentHour);
-	event FundingRateUpdated(int256 fundingRate, int256 fundingRateValue);
-	event InsuranceFundingRateUpdated(
-		int256 insuranceFundingRate,
-		int256 insuranceFundingRateValue
-	);
 	event Deposit(address indexed user, uint256 indexed amount);
 	event Withdraw(address indexed user, uint256 indexed amount);
 	event Settled(address indexed account, int256 margin);
@@ -67,36 +54,35 @@ contract TracerPerpetualSwaps is
 	 *         will be able to purchase and trade tracers after this deployment.
 	 * @param _marketId the id of the market, given as BASE/QUOTE
 	 * @param _tracerBaseToken the address of the token used for margin accounts (i.e. The margin token)
-	 * @param _oracle the address of the contract implementing the tracer oracle interface
 	 * @param _gasPriceOracle the address of the contract implementing gas price oracle
 	 * @param _pricingContract the address of the contract implementing the IPricing.sol interface
+	 * @param _liquidationContract the contract that manages liquidations for this market
+	 * @param _maxLeverage the max leverage of the market. Min margin is derived from this
+	 * @param _fundingRateSensitivity the affect funding rate changes have on funding paid.
+	 * @param _feeRate the fee to be taken on trades in this market
 	 */
 	constructor(
 		bytes32 _marketId,
 		address _tracerBaseToken,
-		address _oracle,
 		address _gasPriceOracle,
 		address _pricingContract,
 		address _liquidationContract,
 		int256 _maxLeverage,
 		uint256 _fundingRateSensitivity,
-		uint256 _feeRate
+		uint256 _feeRate,
+		uint256 _oracleDecimals
 	) public Ownable() {
 		pricingContract = IPricing(_pricingContract);
+		// dont convert to interface as we don't need to interact
+		// with the contract
 		liquidationContract = _liquidationContract;
 		tracerBaseToken = _tracerBaseToken;
-		oracle = _oracle;
 		gasPriceOracle = _gasPriceOracle;
 		marketId = _marketId;
-		IOracle ioracle = IOracle(oracle);
-		priceMultiplier = 10**uint256(ioracle.decimals());
+		priceMultiplier = 10**uint256(_oracleDecimals);
 		feeRate = _feeRate;
 		maxLeverage = _maxLeverage;
 		fundingRateSensitivity = _fundingRateSensitivity;
-
-		// Start average prices from deployment
-		startLastHour = block.timestamp;
-		startLast24Hours = block.timestamp;
 	}
 
 	/**
@@ -146,20 +132,6 @@ contract TracerPerpetualSwaps is
 		emit Withdraw(msg.sender, amount);
 	}
 
-	/**
-	 * @notice Sets the pricing constants initiallly in the pricing contract
-	 */
-	function initializePricing() public override onlyOwner {
-		require(!pricingInitialized, "TCR: Pricing already set ");
-		// Set first funding rates to 0 and current time
-		int256 oracleLatestPrice = IOracle(oracle).latestAnswer();
-		pricingContract.setFundingRate(oracleLatestPrice, 0, 0);
-		pricingContract.setInsuranceFundingRate(oracleLatestPrice, 0, 0);
-
-		pricingContract.incrementFundingIndex();
-		pricingInitialized = true;
-	}
-
 	// TODO: Once whitelisting of trading interfaces is implemented this should
 	// only be called by a whitelisted interface
 	/**
@@ -205,7 +177,7 @@ contract TracerPerpetualSwaps is
 
 		// Update internal trade state
 		// note: price has already been validated here, so order 1 price can be used
-		updateInternalRecords(order1.price);
+		pricingContract.recordTrade(order1.price, fillAmount);
 
 		// Ensures that you are in a position to take the trade
 		require(
@@ -439,67 +411,6 @@ contract TracerPerpetualSwaps is
 		}
 	}
 
-	// todo most of this logic should be in pricing. Tracer should simply
-	// call pricing and let it handle if state needs to be updated
-	/**
-	 * @notice Updates the internal records for pricing, funding rate and interest
-	 * @param price The price to be used to update the internal records, this is the price that a trade occurred at
-	 *              (i.e. The price and order has been filled at)
-	 */
-	function updateInternalRecords(int256 price) internal {
-		IOracle ioracle = IOracle(oracle);
-		if (startLastHour <= block.timestamp - 1 hours) {
-			// emit the old hourly average
-			int256 hourlyTracerPrice =
-				pricingContract.getHourlyAvgTracerPrice(currentHour);
-			emit HourlyPriceUpdated(hourlyTracerPrice, currentHour);
-
-			// Update the price to a new entry and funding rate every hour
-			// Check current hour and loop around if need be
-			if (currentHour == 23) {
-				currentHour = 0;
-			} else {
-				currentHour = currentHour + 1;
-			}
-			// Update pricing and funding rate states
-			pricingContract.updatePrice(price, ioracle.latestAnswer(), true);
-			int256 poolFundingRate =
-				insuranceContract.getPoolFundingRate(address(this)).toInt256();
-
-			pricingContract.updateFundingRate(
-				ioracle.latestAnswer(),
-				poolFundingRate
-			);
-
-			// Gather variables and emit events
-			uint256 currentFundingIndex = pricingContract.currentFundingIndex();
-			(, , int256 fundingRate, int256 fundingRateValue) =
-				pricingContract.getFundingRate(currentFundingIndex);
-			(
-				,
-				,
-				int256 insuranceFundingRate,
-				int256 insuranceFundingRateValue
-			) = pricingContract.getInsuranceFundingRate(currentFundingIndex);
-			emit FundingRateUpdated(fundingRate, fundingRateValue);
-			emit InsuranceFundingRateUpdated(
-				insuranceFundingRate,
-				insuranceFundingRateValue
-			);
-
-			if (startLast24Hours <= block.timestamp - 24 hours) {
-				// Update the interest rate every 24 hours
-				pricingContract.updateTimeValue();
-				startLast24Hours = block.timestamp;
-			}
-
-			startLastHour = block.timestamp;
-		} else {
-			// Update old pricing entry
-			pricingContract.updatePrice(price, ioracle.latestAnswer(), false);
-		}
-	}
-
 	// todo this function should be in a lib
 	/**
 	 * @notice Checks the validity of a potential margin given the necessary parameters
@@ -564,10 +475,6 @@ contract TracerPerpetualSwaps is
 
 	function setPricingContract(address pricing) public override onlyOwner {
 		pricingContract = IPricing(pricing);
-	}
-
-	function setOracle(address _oracle) public override onlyOwner {
-		oracle = _oracle;
 	}
 
 	function setGasOracle(address _gasOracle) public override onlyOwner {
