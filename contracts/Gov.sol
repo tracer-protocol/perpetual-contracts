15a7d042e1f39bef08fe2b77b0b598ccc6c87fc2
raymogg <raymondmogg@gmail.com>
2021-05-04 15:38:39 +1000

    remove explicit abi encoder v2

--- contracts/Gov.sol
+++ contracts/Gov.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
-pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./Interfaces/IGov.sol";
