6db3f154cc7c7e388beae43ee3046082cc6f7552
sporejack <jack.mcpherson@mycelium.ventures>
2021-05-05 10:38:20 +1000

    Make `LibPerpetuals` an actual library
    This commit actually defines a Solidity library for `LibPerpetuals`.

--- contracts/lib/LibPerpetuals.sol
+++ contracts/lib/LibPerpetuals.sol
@@ -1,35 +1,37 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-enum Side {
-    Long,
-    Short
-}
-
-struct Order {
-    address maker;
-    uint256 price;
-    uint256 amount;
-    uint256 filled;
-    Side side;
-    uint256 expires;
-    uint256 created;
-}
-
-struct Signature {
-    bytes32 r;
-    bytes32 s;
-    uint8 v;
-}
-
-struct LimitOrder {
-    Order order;
-    address market;
-    uint256 nonce;
-}
-
-struct SignedLimitOrder {
-    LimitOrder order;
-    Signature signature;
+library Perpetuals {
+    enum Side {
+        Long,
+        Short
+    }
+    
+    struct Order {
+        address maker;
+        uint256 price;
+        uint256 amount;
+        uint256 filled;
+        Side side;
+        uint256 expires;
+        uint256 created;
+    }
+    
+    struct Signature {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+    }
+    
+    struct LimitOrder {
+        Order order;
+        address market;
+        uint256 nonce;
+    }
+    
+    struct SignedLimitOrder {
+        LimitOrder order;
+        Signature signature;
+    }
 }
 
