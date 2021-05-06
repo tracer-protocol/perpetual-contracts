c859ff07c0047cb53307856f856767db301161f0
sporejack <jack.mcpherson@mycelium.ventures>
2021-05-06 15:03:40 +1000

    Add `netValue` function to `LibBalances`
    This commit adds the `netValue` function to the `LibBalances` library.

--- contracts/lib/LibBalances.sol
+++ contracts/lib/LibBalances.sol
@@ -1,9 +1,12 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
+import "./LibMath.sol";
 import "./LibPerpetuals.sol";
 
 library Balances {
+    using LibMath for int256;
+
     struct Position {
         int256 base;
         int256 quote;
@@ -13,6 +16,14 @@ library Balances {
         uint256 price;
         uint256 amount;
         Perpetuals.Side side;
+    }
+
+    function netValue(
+        Position calldata position,
+        uint256 price
+    ) public pure returns (uint256) {
+        /* cast is safe due to semantics of `abs` */
+        return uint256(position.quote.abs()) * price;
     }
 }
 
