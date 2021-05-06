507f5c636aa43858d834abf52940e36f7e88da11
sporejack <72908791+sporejack@users.noreply.github.com>
2021-05-06 14:21:14 +1000

    Base implementation of LibPerpetuals (#56)
    * Create `LibPerpetuals` library
    
    This commit creates a Solidity file for the `LibPerpetuals` library.
    
    * Define `Side` enumerated type
    
    This commit defines the type `Side`, which is an enum representing
    sides of a Tracer market.
    
    * Define `Order` type
    
    This commit defines the `Order` type, which represents an order in a
    Tracer market.
    
    * Define `Signature` type
    
    This commit defines the `Signature` type, which represents the output
    of the ECDSA.
    
    * Define `LimitOrder` type
    
    This commit defines the `LimitOrder` type, which represents a
    user-submitted order.
    
    * Define `SignedLimitOrder` type
    
    This commit defines the `SignedLimitOrder` type, which represents an
    ECDSA-signed limit order.
    
    * Make `LibPerpetuals` an actual library
    
    This commit actually defines a Solidity library for `LibPerpetuals`.
    
    * Modify `Order` definition
    
    This commit changes the `Order` type in two ways:
    
     - Add the `market` field
     - Remove the `filled` field
    
    * Remove (now) redundant types from `LibPerpetuals`
    
    This commit removes the following types from `LibPerpetuals`, as we've
    now decided they belong elsewhere:
    
     - `Signature`
     - `LimitOrder`
     - `SignedLimitOrder`
    
    * Remove `Order` from `Types.sol`
    
    This commit removes the `Order` type from `Types.sol` as it now lives
    in `LibPerpetuals`.
    
    * Fix additional compilation issues
    
    This commit fixes additional compilation issues (*with these changes*)
    by adding an additional function to the `ITrader` interface, amonst
    other things.
    
    * Change `expiration` field to `expires`
    
    This commit fixes a small issue with the Trader shim.
    
    * Add `canMatch` function to `LibPerpetuals`
    
    This commit adds the `canMatch` function to the `LibPerpetuals`
    library. This function implements the logic which determines whether
    two orders (along with their currently-filled amounts) are eligible to
    be matched against each other.
    
    * Use `canMatch`
    
    This commit replaces the previous match-checking logic with `canMatch`.
    
    * Use fresh filled state in `Trader.sol`
    
    This commit uses fresh filled state of orders in `Trader.sol`

--- contracts/Interfaces/ILiquidation.sol
+++ contracts/Interfaces/ILiquidation.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 import "./Types.sol";
+import "../lib/LibPerpetuals.sol";
 import "../lib/LibLiquidation.sol";
 
 interface ILiquidation {
@@ -9,14 +10,14 @@ interface ILiquidation {
 
     function calcAmountToReturn(
         uint256 escrowId,
-        Types.Order[] memory orders,
+        Perpetuals.Order[] memory orders,
         uint256 priceMultiplier,
         address traderContract,
         address liquidator
     ) external returns (uint256);
 
     function calcUnitsSold(
-        Types.Order[] memory orders,
+        Perpetuals.Order[] memory orders,
         address traderContract,
         uint256 receiptId
     ) external returns (uint256, int256);
@@ -33,7 +34,7 @@ interface ILiquidation {
 
     function claimReceipts(
         uint256 receiptId,
-        Types.Order[] memory orders,
+        Perpetuals.Order[] memory orders,
         address traderContract
     ) external;
 
