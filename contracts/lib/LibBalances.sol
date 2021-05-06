9d7523bd3292f14e127d93641d64767be3e330cd
sporejack <jack.mcpherson@mycelium.ventures>
2021-05-06 14:55:40 +1000

    Define `Trade` type
    This commit defines the `Trade` type, which represents a trade in the
    Tracer system.

--- contracts/lib/LibBalances.sol
+++ contracts/lib/LibBalances.sol
@@ -1,10 +1,18 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
+import "./LibPerpetuals.sol";
+
 library Balances {
     struct Position {
         int256 base;
         int256 quote;
+    }
+
+    struct Trade {
+        uint256 price;
+        uint256 amount;
+        Perpetuals.Side side;
     }
 }
 
