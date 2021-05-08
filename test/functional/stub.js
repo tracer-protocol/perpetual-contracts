5665983afd9909e5471854eca510bbdd6aff71cd
sporejack <jack.mcpherson@mycelium.ventures>
2021-05-08 20:43:19 +1000

    Add stub functional test

---
+++ test/functional/stub.js
@@ -0,0 +1,11 @@
+const { expect } = require("chai")
+const { ethers, getNamedAccounts, deployments } = require("hardhat")
+
+describe("Stub test", async () => {
+    context("When run", async () => {
+        it("Passes", async () => {
+            1 + 1;
+        });
+    });
+});
+
