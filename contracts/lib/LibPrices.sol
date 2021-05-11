6badb72a0a38e402211c0e9ed0b4b33bbcd37999
sporejack <jack.mcpherson@mycelium.ventures>
2021-05-11 10:56:05 +1000

    Add `LibPrices` library
    This commit defines the `LibPrices` Solidity library.

---
+++ contracts/lib/LibPrices.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+library Prices {
+    struct FundingRate {
+        uint256 timestamp;
+        int256 fundingRate;
+        int256 cumulativeFundingRate;
+    }
+}
+
