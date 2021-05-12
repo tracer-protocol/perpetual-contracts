a57c4002554d2a8d07a0a1991ce9d5a91351907e
dospore <tetthera@gmail.com>
2021-05-12 15:52:21 +1000

    Add contract deploys to FullDeploy

--- deploy/FullDeploy.js
+++ deploy/FullDeploy.js
@@ -4,17 +4,67 @@ module.exports = async function (hre) {
 
     const { deployer } = await getNamedAccounts()
 
-    const libLiquidation = await deploy("LibLiquidation", {
+    // deploy libs
+    const safetyWithdraw = await deploy('SafetyWithdraw', {
+        from: deployer,
+        log: true,
+    })
+    const libMath = await deploy('LibMath', {
+        from: deployer,
+        log: true,
+    })
+    const libPerpetuals = await deploy('Perpetuals', {
+        from: deployer,
+        log: true,
+    })
+    const libBalances = await deploy('Balances', {
+        from: deployer,
+        log: true,
+    })
+    const libLiquidation = await deploy('LibLiquidation', {
         from: deployer,
         log: true,
     })
 
-    await deploy("LibLiquidationMock", {
+    // deploy trader
+    await deploy('Trader', {
         from: deployer,
         log: true,
         libraries: {
-            LibLiquidation: libLiquidation.address,
+            Perpetuals: libPerpetuals.address
+        }
+    });
+
+    // deploy oracles
+    await deploy('Oracle', {
+        from: deployer,
+        log: true,
+    });
+
+    // deploy token
+    await deploy('TestToken', {
+        args: ["100000000000000000000000"],
+        from: deployer,
+        log: true,
+    });
+
+    // deploy Tracer perps deployer
+    const deployerV1 = await deploy('DeployerV1', {
+        from: deployer,
+        libraries: {
+            Perpetuals: libPerpetuals.address,
+            LibMath: libMath.address,
+            SafetyWithdraw: safetyWithdraw.address,
+            Balances: libBalances.address
         },
-    })
+        log: true,
+    });
+
+    // // deploy Tracer perps factory
+    await deploy('TracerPerpetualsFactory', {
+        args: [deployerV1.address, deployer],
+        from: deployer,
+        log: true,
+    });
 }
-module.exports.tags = ["LibLiquidationMock"]
+module.exports.tags = ["FullDeploy"]
--- scripts/DeployAndAddTracer.js
+++ scripts/DeployAndAddTracer.js
@@ -1,72 +1,54 @@
-const hre = require("hardhat")
-const TestToken = artifacts.require("TestToken")
-const { BN, time } = require("@openzeppelin/test-helpers")
-const Gov = artifacts.require("Gov")
-const TracerFactory = artifacts.require("TracerPerpetualsFactory")
+const hre = require("hardhat");
+const { BN } = require("@openzeppelin/test-helpers");
 
 // small sample script for using deploys and then deploying a trace
 async function main() {
     // deploy all contracts using hardhat deploy
-    const { deployments } = hre
-    const twoDays = 172800
-    let accounts = await web3.eth.getAccounts()
-
+    const { deployments } = hre;
+    const twoDays = 172800;
     // deploy all contracts
-    await deployments.fixture(["FullDeploy"])
+    await deployments.fixture(["FullDeploy"]);
 
-    // todo how do collisions work on  multiple instances of a contract?
-    let oracle = await deployments.get('Oracle');
-    let gasPriceOracle = await deployments.get('Oracle');
-    let account = await deployments.get('Account');
-    let pricing = await deployments.get('Pricing');
     let maxLeverage = new BN("125000").toString();
-    let gov = await deployments.get('Gov');
-    let govToken = await deployments.get('TestToken');
-    let factory = await deployments.get("TracerPerpetualsFactory")
-    let trader = await deployments.get("Trader")
-    gov = await Gov.at(gov.address)
-    govToken = await TestToken.at(govToken.address)
-    factory = await TracerFactory.at(factory.address)
+    let tokenDecimals = new BN("1000000").toString();
+    let fundingRateSensitivity = 1;
+    let feeRate = 1;
+    let gasPriceOracle = await deployments.get('Oracle');
+    // let liquidation = await deployments.get('Liquidation');
+    let trader = await deployments.get("Trader");
+    let factory = await deployments.get("TracerPerpetualsFactory");
+    let token = await deployments.get("TestToken");
 
-    // get the deployed gov token and send it out
-    for (var i = 0; i < 6; i++) {
-        await govToken.transfer(accounts[i], web3.utils.toWei("200"))
-    }
-
-    // use gov to deploy a tracer
-    var token = await TestToken.new(web3.utils.toWei("1000000"))
 
     //Deploy a new Tracer contract per test
-    var deployTracerData = web3.eth.abi.encodeParameters(
-        [
-            "bytes32",
-            "address",
-            "address",
-            "address",
-            "address",
-            "address",
-            "int256",
-            "uint256",
-        ],
-        [
-            web3.utils.fromAscii(`TEST1/USD`),
-            token.address,
-            oracle.address,
-            gasPriceOracle.address,
-            account.address,
-            pricing.address,
-            maxLeverage,
-            1, //funding rate sensitivity
-        ]
-    )
+    // var deployTracerData = ethers.utils.defaultAbiCoder.encode(
+    //     [
+    //         "bytes32", //_marketId,
+    //         "address", //_tracerQuoteToken,
+    //         "uint256", //_tokenDecimals,
+    //         "address", //_gasPriceOracle,
+    //         "address", //_liquidationContract,
+    //         "uint256", //_maxLeverage,
+    //         "uint256", //_fundingRateSensitivity,
+    //         "uint256", //_feeRate
+    //     ],
+    //     [
+    //         ethers.utils.hexlify(ethers.utils.toUtf8Bytes('TEST1/USD')),
+    //         token.address,
+    //         tokenDecimals,
+    //         gasPriceOracle.address,
+    //         liquidation.address,
+    //         maxLeverage,
+    //         fundingRateSensitivity,
+    //         feeRate
+    //     ]
+    // )
 
-    await factory.deployTracer(deployTracerData)
-    let tracerAddr = await factory.tracersByIndex(0)
+    // await factory.deployTracer(deployTracerData)
+    // let tracerAddr = await factory.tracersByIndex(0)
     console.log(`Factory Deplpoyed: ${factory.address}`)
-    console.log(`Account Deplpoyed: ${account.address}`)
-    console.log(`Pricing Deplpoyed: ${pricing.address}`)
+    // console.log(`Tracer Deplpoyed: ${tracerAddr}`)
     console.log(`Trader Deplpoyed: ${trader.address}`)
-    console.log(`Tracer Deplpoyed: ${tracerAddr}`)
     console.log(`Margin Token Deplpoyed: ${token.address}`)
 }
 
