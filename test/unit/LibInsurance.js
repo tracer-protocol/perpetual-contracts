34d3e1fccae55fe4de20eb87a88e0e706d7033d9
raymogg <raymondmogg@gmail.com>
2021-05-13 14:01:50 +1000

    reformat tests

--- test/unit/LibInsurance.js
+++ test/unit/LibInsurance.js
@@ -30,73 +30,93 @@ describe("Unit tests: LibInsurance.sol", function () {
         accounts = await ethers.getSigners()
     })
 
-    context("calcMintAmount", async () => {
-        it("returns 0 if pool token total supply is 0", async () => {
-            let result = await libInsurance.calcMintAmount(zero, zero, zero)
-            expect(result.toString()).to.equal(zero.toString())
+    describe("calcMintAmount", async () => {
+        context("when called with pool token total supply as 0", async () => {
+            it("returns 0", async () => {
+                let result = await libInsurance.calcMintAmount(zero, zero, zero)
+                expect(result.toString()).to.equal(zero.toString())
+            })
         })
 
-        it("returns 0 if the amount to stake is 0", async () => {
-            let result = await libInsurance.calcMintAmount(
-                ethers.utils.parseEther("10"), //pool token supply
-                ethers.utils.parseEther("5"), //collateral held
-                ethers.utils.parseEther("0") //amount of collateral to deposit
-            )
-            expect(result.toString()).to.equal(zero.toString())
+        context("when called with amount to stake as 0", async () => {
+            it("returns 0", async () => {
+                let result = await libInsurance.calcMintAmount(
+                    ethers.utils.parseEther("10"), //pool token supply
+                    ethers.utils.parseEther("5"), //collateral held
+                    ethers.utils.parseEther("0") //amount of collateral to deposit
+                )
+                expect(result.toString()).to.equal(zero.toString())
+            })
         })
 
-        it("returns 0 if the pool token underlying is 0", async () => {
-            let result = await libInsurance.calcMintAmount(
-                ethers.utils.parseEther("10"), //pool token supply
-                ethers.utils.parseEther("0"), //collateral held
-                ethers.utils.parseEther("10") //amount of collateral to deposit
-            )
-            expect(result.toString()).to.equal(zero.toString())
+        context("when called with pool token underlying as 0", async () => {
+            it("returns 0 if the pool token underlying is 0", async () => {
+                let result = await libInsurance.calcMintAmount(
+                    ethers.utils.parseEther("10"), //pool token supply
+                    ethers.utils.parseEther("0"), //collateral held
+                    ethers.utils.parseEther("10") //amount of collateral to deposit
+                )
+                expect(result.toString()).to.equal(zero.toString())
+            })
         })
 
-        it("returns the expected amount", async () => {
-            let expectedResult = ethers.utils.parseEther("20")
-            let result = await libInsurance.calcMintAmount(
-                ethers.utils.parseEther("10"), //pool token supply
-                ethers.utils.parseEther("5"), //collateral held
-                ethers.utils.parseEther("10") //amount of collateral to deposit
-            )
-            expect(result.toString()).to.equal(expectedResult.toString())
+        context("when called with all non zero params", async () => {
+            it("passes", async () => {
+                let expectedResult = ethers.utils.parseEther("20")
+                let result = await libInsurance.calcMintAmount(
+                    ethers.utils.parseEther("10"), //pool token supply
+                    ethers.utils.parseEther("5"), //collateral held
+                    ethers.utils.parseEther("10") //amount of collateral to deposit
+                )
+                expect(result.toString()).to.equal(expectedResult.toString())
+            })
         })
     })
 
-    context("calcWithdrawAmount", async () => {
-        it("returns 0 if pool token total supply is 0", async () => {
-            let result = await libInsurance.calcWithdrawAmount(zero, zero, zero)
-            expect(result.toString()).to.equal(zero.toString())
+    describe("calcWithdrawAmount", async () => {
+        context("when called with pool token total supply as 0", async () => {
+            it("returns 0", async () => {
+                let result = await libInsurance.calcWithdrawAmount(
+                    zero,
+                    zero,
+                    zero
+                )
+                expect(result.toString()).to.equal(zero.toString())
+            })
         })
 
-        it("returns 0 if the amount to withdraw is 0", async () => {
-            let result = await libInsurance.calcWithdrawAmount(
-                ethers.utils.parseEther("10"), //pool token supply
-                ethers.utils.parseEther("5"), //collateral held
-                ethers.utils.parseEther("0") //amount of collateral to deposit
-            )
-            expect(result.toString()).to.equal(zero.toString())
+        context("when called with amount to withdraw as 0", async () => {
+            it("returns 0", async () => {
+                let result = await libInsurance.calcWithdrawAmount(
+                    ethers.utils.parseEther("10"), //pool token supply
+                    ethers.utils.parseEther("5"), //collateral held
+                    ethers.utils.parseEther("0") //amount of collateral to deposit
+                )
+                expect(result.toString()).to.equal(zero.toString())
+            })
         })
 
-        it("returns 0 if the pool token supply is 0", async () => {
-            let result = await libInsurance.calcWithdrawAmount(
-                ethers.utils.parseEther("0"), //pool token supply
-                ethers.utils.parseEther("10"), //collateral held
-                ethers.utils.parseEther("10") //amount of collateral to deposit
-            )
-            expect(result.toString()).to.equal(zero.toString())
+        context("when called with pool token underlying as 0", async () => {
+            it("returns 0", async () => {
+                let result = await libInsurance.calcWithdrawAmount(
+                    ethers.utils.parseEther("5"), //pool token supply
+                    ethers.utils.parseEther("0"), //collateral held
+                    ethers.utils.parseEther("10") //amount of collateral to deposit
+                )
+                expect(result.toString()).to.equal(zero.toString())
+            })
         })
 
-        it("returns the expected amount", async () => {
-            let expectedResult = ethers.utils.parseEther("5")
-            let result = await libInsurance.calcWithdrawAmount(
-                ethers.utils.parseEther("10"), //pool token supply
-                ethers.utils.parseEther("5"), //collateral held
-                ethers.utils.parseEther("10") //amount of pool tokens to withdraw
-            )
-            expect(result.toString()).to.equal(expectedResult.toString())
+        context("when called with all non zero params", async () => {
+            it("passes", async () => {
+                let expectedResult = ethers.utils.parseEther("5")
+                let result = await libInsurance.calcWithdrawAmount(
+                    ethers.utils.parseEther("10"), //pool token supply
+                    ethers.utils.parseEther("5"), //collateral held
+                    ethers.utils.parseEther("10") //amount of pool tokens to withdraw
+                )
+                expect(result.toString()).to.equal(expectedResult.toString())
+            })
         })
     })
 })
