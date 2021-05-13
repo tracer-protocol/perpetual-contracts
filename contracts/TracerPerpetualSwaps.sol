b9ffdc2b5b17bdefb1fc7635f940918d0dd00a47
BenjaminPatch <benpatch@hotmail.com>
2021-05-13 15:26:45 +1000

    Compilation fix

--- contracts/TracerPerpetualSwaps.sol
+++ contracts/TracerPerpetualSwaps.sol
@@ -62,7 +62,6 @@ contract TracerPerpetualSwaps is
      * @param _marketId the id of the market, given as BASE/QUOTE
      * @param _tracerQuoteToken the address of the token used for margin accounts (i.e. The margin token)
      * @param _gasPriceOracle the address of the contract implementing gas price oracle
-     * @param _liquidationContract the contract that manages liquidations for this market
      * @param _maxLeverage the max leverage of the market. Min margin is derived from this
      * @param _fundingRateSensitivity the affect funding rate changes have on funding paid.
      * @param _feeRate the fee to be taken on trades in this market
