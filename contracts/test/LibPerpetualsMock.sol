623697b39abb151e8b40e61cc1875595782a89eb
raymogg <raymondmogg@gmail.com>
2021-05-19 10:20:12 +1000

    add unit tests for lib perps

---
+++ contracts/test/LibPerpetualsMock.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.0;
+
+import "../lib/LibPerpetuals.sol";
+
+contract LibPerpetualsMock {
+     function canMatch(
+        Perpetuals.Order calldata a,
+        uint256 aFilled,
+        Perpetuals.Order calldata b,
+        uint256 bFilled
+    ) public view returns(bool) {
+        return Perpetuals.canMatch(a, aFilled, b, bFilled);
+    }
+}
