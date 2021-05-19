ace395a7c25ec4775682e9c2b53303492647e522
sporejack <jack.mcpherson@mycelium.ventures>
2021-05-19 10:20:00 +1000

    Implement unit test cases for netValue

--- test/unit/LibBalances.js
+++ test/unit/LibBalances.js
@@ -2,27 +2,47 @@ const { expect } = require("chai")
 const { ethers, getNamedAccounts, deployments } = require("hardhat")
 
 describe("Unit tests: LibBalances.sol", async () => {
+    let libBalances
     let accounts
+
+    let normalPosition
+    let minimumPrice
 
     before(async () => {
         await deployments.fixture(["LibBalancesMock"])
         const { deployer } = await getNamedAccounts()
         const deployment = await deployments.get("LibBalancesMock")
-        libLiquidation = await ethers.getContractAt(
+        libBalances = await ethers.getContractAt(
             deployment.abi,
             deployment.address
         )
         accounts = await ethers.getSigners()
+
+        const normalBase = ethers.utils.parseEther("33")
+        const normalQuote = ethers.utils.parseEther("12")
+        normalPosition = [normalQuote, normalBase]
+
+        minimumPrice = ethers.utils.parseEther("0")
+        maximumPrice = ethers.constants.MaxInt256
+        normalPrice = ethers.utils.parseEther("300")
     })
 
     describe("netValue", async () => {
-        /* normal position */
-
         context(
             "When called with normal position and minimum price",
             async () => {
                 it("Returns the correct result", async () => {
-                    /* TODO: implement */
+                    const position = normalPosition
+                    const price = minimumPrice
+
+                    const actualNetValue = await libBalances.netValue(
+                        position,
+                        price
+                    )
+
+                    const expectedNetValue = ethers.utils.parseEther("0")
+
+                    expect(actualNetValue).to.equal(expectedNetValue)
                 })
             }
         )
@@ -30,8 +50,12 @@ describe("Unit tests: LibBalances.sol", async () => {
         context(
             "When called with normal position and maximum price",
             async () => {
-                it("Returns the correct result", async () => {
-                    /* TODO: implement */
+                it("Reverts", async () => {
+                    const position = normalPosition
+                    const price = maximumPrice
+
+                    await expect(libBalances.netValue(position, price)).to.be
+                        .reverted
                 })
             }
         )
@@ -40,7 +64,17 @@ describe("Unit tests: LibBalances.sol", async () => {
             "When called with normal position and normal price",
             async () => {
                 it("Returns the correct result", async () => {
-                    /* TODO: implement */
+                    const position = normalPosition
+                    const price = normalPrice
+
+                    const actualNetValue = await libBalances.netValue(
+                        position,
+                        price
+                    )
+
+                    const expectedNetValue = ethers.utils.parseEther("9900")
+
+                    expect(actualNetValue).to.equal(expectedNetValue)
                 })
             }
         )
