edd0b810d7e7d4161abec69ad17667231dd20e9f
sporejack <72908791+sporejack@users.noreply.github.com>
2021-05-19 15:29:56 +1000

    Base Implementation of `LibPrices` (#81)
    * Add `LibPrices` library
    
    This commit defines the `LibPrices` Solidity library.
    
    * Correct name of funding rate type
    
    * Update `IPricing` interface to use `LibPrices`
    
    This commit modifies the `IPricing` interface to use the new funding
    rate type (`FundingRateInstant`) defined in the `LibPrices` library.
    
    * Remove old funding rate type
    
    * Patch contracts to use new funding rate type
    
    * Fix formatting
    
    * Add `globalLeverage` function to `LibPrices`
    
    This commit adds the `globalLeverage` function to the `LibPrices`
    library. This function calculates the new global leveraged notional
    value from old and new account states.
    
    * Define price instant type
    
    * Patch types file to use new pricing type
    
    * Add TWAP type to LibPrices
    
    * Add `calculateTWAP` to `LibPrices`
    
    This commit implements TWAP calculation in `LibPrices`.
    
    * Add array utilities to maths library
    
    * Implement average pricing for arbitrary periods
    
    * Fix array types in parameters
    
    * Fix array types in LibPrices
    
    * Patch other contracts to use new pricing libs
    
    * Use new pricing lib for 24 hour prices
    
    * Extract fair pricing to pricing library
    
    * Extract time value into LibPrices
    
    * Simplify getters in pricing contract
    
    * Fix formatting
    
    * Prune old types file
    
    * Fix formatting!
    
    * WAD LibPrices
    
    * Fix arithmetic signedness issues in calculating global leverage
    
    * Fix formatting
    
    * Fix casting of delta subtraction
    
    * Replace negative delta with reversed expression
    
    * Refactor global leverage calculation
    
    * Simplify global leverage logic even further

--- contracts/Interfaces/Types.sol
+++ contracts/Interfaces/Types.sol
@@ -2,25 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "../lib/LibPerpetuals.sol";
+import "../lib/LibPrices.sol";
 
 interface Types {
-    struct FundingRate {
-        uint256 recordTime;
-        uint256 recordPrice;
-        int256 fundingRate; //positive value = longs pay shorts
-        int256 fundingRateValue; //previous rate + (time diff * price * rate)
-    }
-
-    struct HourlyPrices {
-        uint256 totalPrice;
-        uint256 numTrades;
-    }
-
-    struct PricingMetrics {
-        Types.HourlyPrices[24] hourlyTracerPrices;
-        Types.HourlyPrices[24] hourlyOraclePrices;
-    }
-
     struct SignedLimitOrder {
         Perpetuals.Order order;
         uint256 nonce;
