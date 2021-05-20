73fb15eca5bfc11b307efdd4ab68a668f7c0d3ba
dospore <tetthera@gmail.com>
2021-05-20 15:22:32 +1000

    lint

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require("hardhat-contract-sizer")
 require("hardhat-deploy")
 require("hardhat-prettier")
 require("hardhat-typechain")
-require('hardhat-abi-exporter');
+require("hardhat-abi-exporter")
 
 module.exports = {
     solidity: {
