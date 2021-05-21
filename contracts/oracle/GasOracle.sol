f0f71673e7fad0e7dededa14134a8438e52f587d
BenjaminPatch <benpatch@hotmail.com>
2021-05-21 13:24:00 +1000

    Remove old gas price code

--- contracts/oracle/GasOracle.sol
+++ contracts/oracle/GasOracle.sol
@@ -16,8 +16,7 @@ contract GasOracle is IOracle, Ownable {
     using LibMath for uint256;
     IChainlinkOracle public gasOracle;
     IChainlinkOracle public priceOracle;
-    uint256 public usdToGas;
-    uint8 public override decimals = 8; // default of 8 decimals for USD price feeds in the Chainlink ecosystem
+    uint8 public override decimals = 18;
     uint256 private constant MAX_DECIMALS = 18;
 
     constructor(address _priceOracle, address _gasOracle) {
@@ -30,32 +29,12 @@ contract GasOracle is IOracle, Ownable {
      * @dev Returned value is USD/Gas * 10^18 for compatibility with rest of calculations
      */
     function latestAnswer() external view override returns (uint256) {
-        if (usdToGas != 0) {
-            // Default value has been manually set
-            return usdToGas;
-        }
-        uint256 gweiDividor = 9;
-        uint256 ten = 10; // Needed to allow compiler to exponentiate
-
-        uint256 gasDecimals = gasOracle.decimals();
-        uint256 priceDecimals = priceOracle.decimals();
-        uint256 divisionPower =
-            ten**((gasDecimals + priceDecimals) - gweiDividor);
-
-        // todo sanity check on casting. int256->uint256 is safe
-        return ((uint256(gasOracle.latestAnswer()) *
-            uint256(priceOracle.latestAnswer())) / divisionPower);
-
         uint256 gasPrice = toWad(uint256(gasOracle.latestAnswer()), gasOracle);
         uint256 ethPrice =
             toWad(uint256(priceOracle.latestAnswer()), priceOracle);
 
         uint256 result = PRBMathUD60x18.mul(gweiToWei(gasPrice), ethPrice);
         return result;
-
-        // todo sanity check on casting. int256->uint256 is safe
-        return ((uint256(gasOracle.latestAnswer()) *
-            uint256(priceOracle.latestAnswer())) / divisionPower);
     }
 
     /**
@@ -83,13 +62,6 @@ contract GasOracle is IOracle, Ownable {
         require(_decimals <= MAX_DECIMALS, "GAS: too many decimals");
         uint256 scaler = uint256(10**(MAX_DECIMALS - _decimals));
         return raw * scaler;
-    }
-
-    /**
-     * @notice Manually set the new ratio of USD/Gas
-     */
-    function setUsdToGas(uint256 _price) public {
-        usdToGas = _price;
     }
 
     function setGasOracle(address _gasOracle) public onlyOwner {
