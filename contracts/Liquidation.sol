d301aa73ff3c00d8e0e8fc8d7491b0b5b4bbe56a
sporejack <jack.mcpherson@mycelium.ventures>
2021-05-27 19:18:05 +1000

    Convert liquidation events to Side enum

--- contracts/Liquidation.sol
+++ contracts/Liquidation.sol
@@ -48,7 +48,7 @@ contract Liquidation is ILiquidation, Ownable {
         address indexed account,
         address indexed liquidator,
         int256 liquidationAmount,
-        bool side,
+        Perpetuals.Side side,
         address indexed market,
         uint256 liquidationId
     );
@@ -291,7 +291,11 @@ contract Liquidation is ILiquidation, Ownable {
             account,
             msg.sender,
             amount,
-            (liquidatedBalance.position.base < 0 ? false : true),
+            (
+                liquidatedBalance.position.base < 0
+                    ? Perpetuals.Side.Short
+                    : Perpetuals.Side.Long
+            ),
             address(tracer),
             currentLiquidationId - 1
         );
