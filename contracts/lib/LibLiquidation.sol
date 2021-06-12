6497f5cb3ffa1f81d97140a8c14dd30b9b5f07c4
BenjaminPatch <benpatch@hotmail.com>
2021-06-12 20:25:46 +1000

    Comments added

--- contracts/lib/LibLiquidation.sol
+++ contracts/lib/LibLiquidation.sol
@@ -126,11 +126,13 @@ library LibLiquidation {
             if (amountToReturn <= 0) {
                 return 0;
             }
-            // multiply by 100 as we expect this to be a percent as an integer eg 50% = 50
+
+            // slippage percent = slippage / total amount
             percentSlippage = PRBMathUD60x18.div(
                 amountToReturn,
                 amountExpectedFor
             );
+
             if (percentSlippage > maxSlippage) {
                 amountToReturn = PRBMathUD60x18.mul(
                     maxSlippage,
