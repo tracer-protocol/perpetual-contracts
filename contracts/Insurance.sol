2bc16641091c9c0d2ea00898e76b5fc4ad2a44c4
Madhav Kumar <47245900+kumar-ish@users.noreply.github.com>
2021-06-19 04:02:40 +1000

    Fix drainPool public collateral withdrawal
    Previously returning amount left in pool overall; now returning how much left in public

--- contracts/Insurance.sol
+++ contracts/Insurance.sol
@@ -170,7 +170,7 @@ contract Insurance is IInsurance, Ownable, SafetyWithdraw {
                 publicCollateralAmount = 10**18;
             } else {
                 // Take out what you need from the public pool; there's enough for there to be >= 1 token left
-                publicCollateralAmount = poolHoldings - amount;
+                publicCollateralAmount = publicCollateralAmount - amount;
             }
 
             // Drain buffer
