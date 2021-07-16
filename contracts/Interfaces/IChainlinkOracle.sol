0afd85cc0d8440022a6473a69a2b3e27759d8ebe
Osman Branjerdporn <n10138421@qut.edu.au>
2021-07-16 10:38:36 +1000

    fix linter errors

--- contracts/Interfaces/IChainlinkOracle.sol
+++ contracts/Interfaces/IChainlinkOracle.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 /**
- * This interface follows the AggregatorV3 interface. The getRoundData has been excluded as only the 
+ * This interface follows the AggregatorV3 interface. The getRoundData has been excluded as only the
  * latestRoundData function is used in the system to fetch answers.
  * https://github.com/smartcontractkit/chainlink/blob/develop/evm-contracts/src/v0.8/interfaces/AggregatorV3Interface.sol
  * Before being used by the system, any Chainlink feeds that do not provide answers in WAD format (18 decimals) should be wrapped in a
