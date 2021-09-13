a79b0beaec8db2a4e222a9a8a5fad352f1c15c5c
Osman Branjerdporn <n10138421@qut.edu.au>
2021-09-14 09:10:26 +1000

    feat: Test utils

--- test/util/DeploymentUtil.js
+++ test/util/DeploymentUtil.js
@@ -7,59 +7,73 @@ const liquidationAbi = require("../../abi/contracts/Liquidation.sol/Liquidation.
 const tokenAbi = require("../../abi/contracts/TestToken.sol/TestToken.json")
 const oracleAbi = require("../../abi/contracts/oracle/ChainlinkOracle.sol/ChainlinkOracle.json")
 
+const getFactory = async () => {
+    const factory = await deployments.get("TracerPerpetualsFactory")
+    return await ethers.getContractAt(factory.abi, factory.address)
+}
+
+const getTracer = async (factory) => {
+    const tracerAddress = await factory.tracersByIndex(0)
+    return await ethers.getContractAt(tracerAbi, tracerAddress)
+}
+
+const getInsurance = async (tracer) => {
+    const Insurance = await tracer.insuranceContract()
+    return await ethers.getContractAt(insuranceAbi, Insurance)
+}
+
+const getPricing = async (tracer) => {
+    const Pricing = await tracer.pricingContract()
+    return await ethers.getContractAt(pricingAbi, Pricing)
+}
+
+const getLiquidation = async (tracer) => {
+    const Liquidation = await tracer.liquidationContract()
+    return await ethers.getContractAt(liquidationAbi, Liquidation)
+}
+
+const getQuoteToken = async (tracer) => {
+    const QuoteToken = await tracer.tracerQuoteToken()
+    return await ethers.getContractAt(tokenAbi, QuoteToken)
+}
+
+const getTrader = async () => {
+    const traderDeployment = await deployments.get("Trader")
+    return await ethers.getContractAt(
+        traderDeployment.abi,
+        traderDeployment.address
+    )
+}
+
+const getPriceOracle = async () => {
+    const Oracle = await deployments.get("PriceOracle")
+    return await ethers.getContractAt(oracleAbi, Oracle.address)
+}
+
+const getGasEthOracle = async () => {
+    const gasEthOracle = await deployments.get("EthOracle")
+    return await ethers.getContractAt(oracleAbi, gasEthOracle.address)
+}
+
 const deployTracer = deployments.createFixture(async () => {
     const { _deployer } = await getNamedAccounts()
 
     // deploy contracts
     await deployments.fixture(["FullDeployTest"])
-    let factoryInstance = await deployments.get("TracerPerpetualsFactory")
-    let _factory = await ethers.getContractAt(
-        factoryInstance.abi,
-        factoryInstance.address
-    )
-    let tracerAddress = await _factory.tracersByIndex(0)
-    let _tracer = await ethers.getContractAt(tracerAbi, tracerAddress)
-
-    // setup mocks for the contracts and relink
-    const Insurance = await _tracer.insuranceContract()
-    let _insurance = await ethers.getContractAt(insuranceAbi, Insurance)
-
-    const Pricing = await _tracer.pricingContract()
-    let _pricing = await ethers.getContractAt(pricingAbi, Pricing)
-
-    const Liquidation = await _tracer.liquidationContract()
-    let _liquidation = await ethers.getContractAt(liquidationAbi, Liquidation)
-
-    const QuoteToken = await _tracer.tracerQuoteToken()
-    let _quoteToken = await ethers.getContractAt(tokenAbi, QuoteToken)
-
-    const traderDeployment = await deployments.get("Trader")
-    let _trader = await ethers.getContractAt(
-        traderDeployment.abi,
-        traderDeployment.address
-    )
-
-    const Oracle = await deployments.get("PriceOracle")
-    let _oracle = await ethers.getContractAt(oracleAbi, Oracle.address)
-
-    // mock ETH / USD oracle used to set gas price
-    const gasEthOracle = await deployments.get("EthOracle")
-    let _gasEthOracle = await ethers.getContractAt(
-        oracleAbi,
-        gasEthOracle.address
-    )
+    _factory = await getFactory()
+    _tracer = await getTracer(_factory)
 
     return {
         deployer: _deployer,
-        tracer: _tracer,
-        insurance: _insurance,
-        pricing: _pricing,
-        liquidation: _liquidation,
-        quoteToken: _quoteToken,
         factory: _factory,
-        trader: _trader,
-        oracle: _oracle,
-        gasEthOracle: _gasEthOracle,
+        tracer: _tracer,
+        insurance: await getInsurance(_tracer),
+        pricing: await getPricing(_tracer),
+        liquidation: await getLiquidation(_tracer),
+        quoteToken: await getQuoteToken(_tracer),
+        trader: await getTrader(),
+        oracle: await getPriceOracle(),
+        gasEthOracle: await getGasEthOracle(),
     }
 })
 
@@ -120,6 +134,15 @@ const deployTracerMockPricing = deployments.createFixture(async () => {
 })
 
 module.exports = {
+    getFactory,
+    getTracer,
+    getInsurance,
+    getPricing,
+    getLiquidation,
+    getQuoteToken,
+    getTrader,
+    getPriceOracle,
+    getGasEthOracle,
     deployTracer,
     deployTracerMockPricing,
 }
