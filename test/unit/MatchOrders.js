512e2016011dfa95ae8ef1c6d378f29136b578a4
Osman Branjerdporn <n10138421@qut.edu.au>
2021-09-14 15:21:52 +1000

    refactor: MatchOrders test cleanup

--- test/unit/MatchOrders.js
+++ test/unit/MatchOrders.js
@@ -18,6 +18,9 @@ const {
     setGasPrice,
 } = require("../util/OrderUtil.js")
 
+const defaultMarkPrice = 2 * 10 ** 8 // 2 USD/ETH
+const defaultGasPrice = 0.00000002 // 20 Gwei
+
 const setupTests = deployments.createFixture(async () => {
     await deployments.fixture(["FullDeployTest"])
     _factory = await getFactory()
@@ -49,7 +52,7 @@ const setupTestsWithMockPricing = deployments.createFixture(async () => {
 })
 
 describe("Unit tests: matchOrders", function () {
-    let accounts
+    let accounts, long, short
     let tracer, trader, pricing, quoteToken, oracle, gasEthOracle
     let initialQuoteBalance, orderPrice, orderAmount
     let tx
@@ -59,22 +62,26 @@ describe("Unit tests: matchOrders", function () {
             ;({ tracer, trader, pricing, quoteToken, oracle, gasEthOracle } =
                 await setupTests())
             accounts = await ethers.getSigners()
+            long = accounts[1]
+            short = accounts[2]
 
-            // set fee rate to 2%
-            await tracer.setFeeRate(ethers.utils.parseEther("0.02"))
-            // set mark price to 2 (oracle takes in 8 decimal answer)
-            await oracle.setPrice(2 * 10 ** 8)
-            await gasEthOracle.setPrice(2 * 10 ** 8)
-
+            // initial balances
+            // long: quote: 10, base: 0
+            // short: quote: 10, base: 0
+            await oracle.setPrice(defaultMarkPrice)
+            await gasEthOracle.setPrice(defaultMarkPrice)
             initialQuoteBalance = ethers.utils.parseEther("10")
             await depositQuoteTokens(
                 tracer,
                 quoteToken,
-                [accounts[1], accounts[2]],
+                [long, short],
                 initialQuoteBalance
             )
 
-            // match order from acc 1 (long) and acc 2 (short)
+            // match orders with 2% fee
+            // long: price: 2, amount: 10
+            // short: price: 2, amount: 10
+            await tracer.setFeeRate(ethers.utils.parseEther("0.02"))
             orderPrice = ethers.utils.parseEther("2")
             orderAmount = ethers.utils.parseEther("10")
             await executeTrade(
@@ -96,11 +103,11 @@ describe("Unit tests: matchOrders", function () {
             const expectedShortQuote = ethers.utils.parseEther("29.6")
             const expectedShortBase = ethers.utils.parseEther("-10")
 
-            const longUser = await tracer.balances(accounts[1].address)
-            const shortUser = await tracer.balances(accounts[2].address)
+            const longBalance = await tracer.balances(long.address)
+            const shortUser = await tracer.balances(short.address)
 
-            expect(longUser.position.quote).to.equal(expectedLongQuote)
-            expect(longUser.position.base).to.equal(expectedLongBase)
+            expect(longBalance.position.quote).to.equal(expectedLongQuote)
+            expect(longBalance.position.base).to.equal(expectedLongBase)
             expect(shortUser.position.quote).to.equal(expectedShortQuote)
             expect(shortUser.position.base).to.equal(expectedShortBase)
         })
@@ -123,11 +130,15 @@ describe("Unit tests: matchOrders", function () {
             const expectedLongLeverage = ethers.utils.parseEther("10.4")
             const expectedShortLeverage = ethers.utils.parseEther("10.4")
 
-            const long = await tracer.balances(accounts[1].address)
-            const short = await tracer.balances(accounts[2].address)
+            const longBalance = await tracer.balances(long.address)
+            const shortBalance = await tracer.balances(short.address)
 
-            expect(long.totalLeveragedValue).to.equal(expectedLongLeverage)
-            expect(short.totalLeveragedValue).to.equal(expectedShortLeverage)
+            expect(longBalance.totalLeveragedValue).to.equal(
+                expectedLongLeverage
+            )
+            expect(shortBalance.totalLeveragedValue).to.equal(
+                expectedShortLeverage
+            )
         })
 
         it("records the trade in the pricing contract", async () => {
@@ -149,17 +160,21 @@ describe("Unit tests: matchOrders", function () {
             ;({ tracer, trader, pricing, quoteToken, gasEthOracle } =
                 await setupTestsWithMockPricing())
             accounts = await ethers.getSigners()
+            long = accounts[1]
+            short = accounts[2]
 
+            // set initial balances
+            // long: quote: 10, base: 1
+            // short: quote: 10, base: -1
             initialQuoteBalance = ethers.utils.parseEther("11")
             await depositQuoteTokens(
                 tracer,
                 quoteToken,
-                [accounts[1], accounts[2]],
+                [long, short],
                 initialQuoteBalance
             )
 
-            // give account 1 a base of 1, quote is now 10
-            await setGasPrice(gasEthOracle, 0.00000002)
+            await setGasPrice(gasEthOracle, defaultGasPrice)
             let markPrice = 1
             await pricing.setFairPrice(
                 ethers.utils.parseEther(markPrice.toString())
@@ -174,11 +189,14 @@ describe("Unit tests: matchOrders", function () {
                 tradeAmount
             )
 
-            // set new funding rate to 0.2 quote tokens per 1 base held at index 1, no insurance rate
+            // set new funding rate to 0.2 quote tokens per 1 base held at index 1
             const fundingRate = ethers.utils.parseEther("0.2")
             await pricing.setFundingRate(1, fundingRate, fundingRate)
             await pricing.setLastUpdatedFundingIndex(1)
 
+            // match orders with fee of 2%
+            // long: Price: 2, Amount: 10
+            // short: Price: 2, Amount: 10
             tradePrice = ethers.utils.parseEther(markPrice.toString())
             tradeAmount = ethers.utils.parseEther("5")
             let tx = await executeTrade(
@@ -189,7 +207,7 @@ describe("Unit tests: matchOrders", function () {
                 tradeAmount
             )
 
-            const postBalance = await tracer.balances(accounts[1].address)
+            const postBalance = await tracer.balances(long.address)
 
             // expected quote = initial quote - initial trade - funding payment - new trade
             // = 11 - 1 - 0.2 - 5 = 4.8
@@ -204,17 +222,21 @@ describe("Unit tests: matchOrders", function () {
             ;({ tracer, trader, pricing, quoteToken, gasEthOracle } =
                 await setupTestsWithMockPricing())
             accounts = await ethers.getSigners()
+            long = accounts[1]
+            short = accounts[2]
 
+            // set initial balances
+            // long: quote: 10, base: 1
+            // short: quote: 10, base: -1
             initialQuoteBalance = ethers.utils.parseEther("11")
             await depositQuoteTokens(
                 tracer,
                 quoteToken,
-                [accounts[1], accounts[2]],
+                [long, short],
                 initialQuoteBalance
             )
 
-            // give account 1 a base of 1, quote is now 10
-            await setGasPrice(gasEthOracle, 0.00000002)
+            await setGasPrice(gasEthOracle, defaultGasPrice)
             let markPrice = 1
             await pricing.setFairPrice(
                 ethers.utils.parseEther(markPrice.toString())
@@ -229,13 +251,16 @@ describe("Unit tests: matchOrders", function () {
                 tradeAmount
             )
 
-            // set new funding rate to 0.2 quote tokens per 1 base held at index 1, no insurance rate
+            // set new funding rate to 0.2 quote tokens per 1 base held at index 1
             const fundingRate = ethers.utils.parseEther("0.2")
             await pricing.setFundingRate(1, fundingRate, fundingRate)
             await pricing.setLastUpdatedFundingIndex(1)
 
+            // match orders with fee of 2%
+            // long: Price: 2, Amount: 1000, insufficient margin
+            // short: Price: 2, Amount: 1000
             tradePrice = ethers.utils.parseEther(markPrice.toString())
-            tradeAmount = ethers.utils.parseEther("1000") // user will have insufficient margin for this trade
+            tradeAmount = ethers.utils.parseEther("1000")
             let tx = await executeTrade(
                 tracer,
                 trader,
@@ -244,7 +269,7 @@ describe("Unit tests: matchOrders", function () {
                 tradeAmount
             )
 
-            const postBalance = await tracer.balances(accounts[1].address)
+            const postBalance = await tracer.balances(long.address)
 
             // expected quote = initial quote - initial trade - funding payment
             // = 11 - 1 - 0.2 = 9.8
@@ -261,38 +286,46 @@ describe("Unit tests: matchOrders", function () {
             ;({ tracer, trader, quoteToken, oracle, gasEthOracle } =
                 await setupTests())
             accounts = await ethers.getSigners()
-            // set mark price to 2 (oracle takes in 8 decimal answer)
-            await oracle.setPrice(2 * 10 ** 8)
-            await gasEthOracle.setPrice(2 * 10 ** 8)
+            long = accounts[1]
+            short = accounts[2]
 
+            await oracle.setPrice(defaultMarkPrice)
+            await gasEthOracle.setPrice(defaultMarkPrice)
+
+            // set initial balances
+            // long: quote: 10, base: 0
+            // short: quote: 10, base: 0
             initialQuoteBalance = ethers.utils.parseEther("10")
             await depositQuoteTokens(
                 tracer,
                 quoteToken,
-                [accounts[1], accounts[2]],
+                [long, short],
                 initialQuoteBalance
             )
 
-            // create order where prices don't cross i.e long price < short price
+            // match orders where prices don't cross
+            // long price < short price
+            // long: Price: 1, Amount: 1
+            // short: Price: 2, Amount: 1
             const longPrice = ethers.utils.parseEther("1")
             const shortPrice = ethers.utils.parseEther("2")
             orderAmount = ethers.utils.parseEther("1")
-            const long = createOrder(
+            const longOrder = createOrder(
                 tracer,
                 longPrice,
                 orderAmount,
                 true,
-                accounts[1].address
+                long.address
             )
-            const short = createOrder(
+            const shortOrder = createOrder(
                 tracer,
                 shortPrice,
                 orderAmount,
                 false,
-                accounts[2].address
+                short.address
             )
 
-            tx = await matchOrders(trader, long, short)
+            tx = await matchOrders(trader, longOrder, shortOrder)
         })
 
         it("does not change user positions", async () => {
@@ -301,11 +334,11 @@ describe("Unit tests: matchOrders", function () {
             const expectedShortQuote = initialQuoteBalance
             const expectedShortBase = 0
 
-            const longUser = await tracer.balances(accounts[1].address)
-            const shortUser = await tracer.balances(accounts[2].address)
+            const longBalance = await tracer.balances(long.address)
+            const shortUser = await tracer.balances(short.address)
 
-            expect(longUser.position.quote).to.equal(expectedLongQuote)
-            expect(longUser.position.base).to.equal(expectedLongBase)
+            expect(longBalance.position.quote).to.equal(expectedLongQuote)
+            expect(longBalance.position.base).to.equal(expectedLongBase)
             expect(shortUser.position.quote).to.equal(expectedShortQuote)
             expect(shortUser.position.base).to.equal(expectedShortBase)
         })
@@ -320,25 +353,29 @@ describe("Unit tests: matchOrders", function () {
             ;({ tracer, trader, quoteToken, oracle, gasEthOracle } =
                 await setupTests())
             accounts = await ethers.getSigners()
+            long = accounts[1]
+            short = accounts[2]
 
-            // set fee rate to 2%
             await tracer.setFeeRate(ethers.utils.parseEther("0.02"))
-            // set mark price to 2 (oracle takes in 8 decimal answer)
-            await oracle.setPrice(2 * 10 ** 8)
-            await gasEthOracle.setPrice(2 * 10 ** 8)
+            await oracle.setPrice(defaultMarkPrice)
+            await gasEthOracle.setPrice(defaultMarkPrice)
 
+            // set initial balances
+            // long: quote: 10, base: 0
+            // short: quote: 10, base: 0
             initialQuoteBalance = ethers.utils.parseEther("10")
             await depositQuoteTokens(
                 tracer,
                 quoteToken,
-                [accounts[1], accounts[2]],
+                [long, short],
                 initialQuoteBalance
             )
 
-            // match order with amount 100
-            // min margin = quote change / max leverage + liq. gas cost (negligble)
-            // min margin = 20 / 12.5 = 16
-            // actual margin = -194 + (100 * 2) = 6
+            // match orders
+            // long: Price: 2, Amount: 100
+            // short: Price: 2, Amount: 100
+            // minimum margin ~= 8
+            // actual long margin = -94
             orderPrice = ethers.utils.parseEther("2")
             orderAmount = ethers.utils.parseEther("100")
             tx = await executeTrade(
@@ -356,11 +393,11 @@ describe("Unit tests: matchOrders", function () {
             const expectedShortQuote = initialQuoteBalance
             const expectedShortBase = 0
 
-            const longUser = await tracer.balances(accounts[1].address)
-            const shortUser = await tracer.balances(accounts[2].address)
+            const longBalance = await tracer.balances(long.address)
+            const shortUser = await tracer.balances(short.address)
 
-            expect(longUser.position.quote).to.equal(expectedLongQuote)
-            expect(longUser.position.base).to.equal(expectedLongBase)
+            expect(longBalance.position.quote).to.equal(expectedLongQuote)
+            expect(longBalance.position.base).to.equal(expectedLongBase)
             expect(shortUser.position.quote).to.equal(expectedShortQuote)
             expect(shortUser.position.base).to.equal(expectedShortBase)
         })
