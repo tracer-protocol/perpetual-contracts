f2bd1bed4c566f291020f5d80800ad469a6cd6a8
Osman Branjerdporn <n10138421@qut.edu.au>
2021-09-16 16:47:33 +1000

    refactor: Name accounts liquidation admin

--- test/unit/LiquidationAdmin.js
+++ test/unit/LiquidationAdmin.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai")
-const { ethers, deployments } = require("hardhat")
+const { ethers, deployments, getNamedAccounts } = require("hardhat")
 const {
     getFactory,
     getTracer,
@@ -17,27 +17,33 @@ const setupTests = deployments.createFixture(async () => {
 })
 
 describe("Unit tests: Liquidation.sol admin", async () => {
+    let deployer
+    before(async function () {
+        accounts = await ethers.getSigners()
+        deployer = accounts[0]
+    })
+
     context("setReleaseTime", async () => {
         context("releaseTime", async () => {
             it("correctly updates ", async () => {
                 const { liquidation } = await setupTests()
-                const accounts = await ethers.getSigners()
+
                 // set multiplier as 20 minutes
                 let newReleaseTime = 20 * 60
                 await liquidation
-                    .connect(accounts[0])
+                    .connect(deployer)
                     .setReleaseTime(newReleaseTime)
                 expect(await liquidation.releaseTime()).to.equal(newReleaseTime)
             })
 
             it("emits an event", async () => {
                 const { liquidation } = await setupTests()
-                const accounts = await ethers.getSigners()
+
                 // set multiplier as 20 minutes
                 let newReleaseTime = 20 * 60
                 expect(
                     await liquidation
-                        .connect(accounts[0])
+                        .connect(deployer)
                         .setReleaseTime(newReleaseTime)
                 )
                     .to.emit(liquidation, "ReleaseTimeUpdated")
@@ -50,11 +56,11 @@ describe("Unit tests: Liquidation.sol admin", async () => {
         context("maxSlippage", async () => {
             it("correctly updates ", async () => {
                 const { liquidation } = await setupTests()
-                const accounts = await ethers.getSigners()
+
                 // set multiplier as 8
                 let newMultiplier = 8
                 await liquidation
-                    .connect(accounts[0])
+                    .connect(deployer)
                     .setMinimumLeftoverGasCostMultiplier(newMultiplier)
                 expect(
                     await liquidation.minimumLeftoverGasCostMultiplier()
@@ -63,12 +69,12 @@ describe("Unit tests: Liquidation.sol admin", async () => {
 
             it("emits an event", async () => {
                 const { liquidation } = await setupTests()
-                const accounts = await ethers.getSigners()
+
                 // set multiplier as 8
                 let newMultiplier = 8
                 expect(
                     await liquidation
-                        .connect(accounts[0])
+                        .connect(deployer)
                         .setMinimumLeftoverGasCostMultiplier(newMultiplier)
                 )
                     .to.emit(
@@ -84,23 +90,23 @@ describe("Unit tests: Liquidation.sol admin", async () => {
         context("maxSlippage", async () => {
             it("correctly updates ", async () => {
                 const { liquidation } = await setupTests()
-                const accounts = await ethers.getSigners()
+
                 // set max slippage as 50%
                 let newMaxSlippage = ethers.utils.parseEther("0.5")
                 await liquidation
-                    .connect(accounts[0])
+                    .connect(deployer)
                     .setMaxSlippage(newMaxSlippage)
                 expect(await liquidation.maxSlippage()).to.equal(newMaxSlippage)
             })
 
             it("emits an event", async () => {
                 const { liquidation } = await setupTests()
-                const accounts = await ethers.getSigners()
+
                 // set max slippage as 50%
                 let newMaxSlippage = ethers.utils.parseEther("0.5")
                 expect(
                     await liquidation
-                        .connect(accounts[0])
+                        .connect(deployer)
                         .setMaxSlippage(newMaxSlippage)
                 )
                     .to.emit(liquidation, "MaxSlippageUpdated")
@@ -111,11 +117,11 @@ describe("Unit tests: Liquidation.sol admin", async () => {
         context("when max slippage is greater than 100%", async () => {
             it("reverts", async () => {
                 const { liquidation } = await setupTests()
-                const accounts = await ethers.getSigners()
+
                 // set max slippage as 123%
                 await expect(
                     liquidation
-                        .connect(accounts[0])
+                        .connect(deployer)
                         .setMaxSlippage(ethers.utils.parseEther("123"))
                 ).to.be.revertedWith("LIQ: Invalid max slippage")
             })
